dd7365ca1b253b8e52fd208699c7875badc94e16
Mediacom <bertoliedoardo99@gmail.com>
2024-10-21 18:20:26 +0200

    I decided I will only deal with jpg or png and create a function that can convert from one format to the other. For now we can encode and decode jpg images and we can convert to the right type of image (RGBA) by redrawing the input image. This lets us have access to the image.Set function that can change pixels.

--- image.go
+++ image.go
@@ -3,17 +3,76 @@ package main
 import (
 	"fmt"
 	"image"
-	_ "image/jpeg" // we could initialize the decoder instead of importing this
-	_ "image/png"
+	"image/jpeg"
+	"image/draw"
 	"log"
 	"os"
 )
 
-func printPixels(image image.Image) {
-	image_bounds := image.Bounds()
+//FIXME
+//there must be a better way
+func logfat(err error, msg string) {
+	if err != nil {
+		log.Fatal(msg, err)
+	}
+}
+
+// We deal with jpeg, make a function to turn png into jpeg
+func convertJpegToPng() error {
+	return nil
+}
+
+// Should check and initialize properly for jpeg and png
+// considering for jpeg I have to create a new image of type RGBA
+// and redraw the original that was an image.YCbCr
+// For now I only handle jpeg images. I will add a function to convert
+// a png to a jpeg image.
+
+func main() {
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+
+	args := os.Args
+	if(len(args) < 2) {
+		fmt.Print("Not enough arguments\nUsage: ./image <image-name>.png/jpeg\n")
+		os.Exit(1)
+	}
+	
+	data, open_err := os.Open(args[1])
+	logfat(open_err, "Could not open image:")
+	defer data.Close()
+	
+	// JPEG decode
+	data_image, decode_err := jpeg.Decode(data)
+	logfat(decode_err, "Error decoding input image:")
+	
+	log.Println("Image bounds:", data_image.Bounds().Max)
+	log.Printf("%T\n", data_image)
+
+	new_image := RedrawImageIntoRgba(data_image)
+
+	encode_err := EncodeRgbaToJpeg(&new_image)
+	logfat(encode_err, "Error encoding image data into jpeg image:")
+}
+
+func EncodeRgbaToJpeg(image_data image.Image) error {	
+	//Create new file to hold new image
+	outfile, open_err := os.Create("NEWIMAGE.jpeg")
+	if open_err != nil { return open_err }
+
+	// JPEG encode
+	encode_err := jpeg.Encode(outfile, image_data, nil)
+	if encode_err != nil { return encode_err }
+	
+	defer outfile.Close()
+	return nil
+}
+
+// Prints every pixel
+func PrintPixels(image_data image.Image) {
+	image_bounds := image_data.Bounds()
 	for y := image_bounds.Min.Y; y < image_bounds.Max.Y; y++ {
 		for x := image_bounds.Min.X; x < image_bounds.Max.X; x++ {
-			r, g, b, a := image.At(x, y).RGBA()
+			r, g, b, a := image_data.At(x, y).RGBA()
 			log.Printf("Pixel at (%d, %d): R: %d, G: %d, B: %d, A: %d\n",
 				x,
 				y,
@@ -25,33 +84,13 @@ func printPixels(image image.Image) {
 	}
 }
 
-func main() {
-	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
-
-	args := os.Args
-	if(len(args) < 2) {
-		fmt.Print("Not enough arguments\nUsage: ./image <image-name>.png/jpeg\n")
-		os.Exit(1)
-	}
-	//TODO should check correct name and format like : <not too long name>.jpeg/png
-	// even if os.Open checks for existence of file with given path
+//Redraws an image into a image.RGBA that is mutable
+func RedrawImageIntoRgba(image_data image.Image) image.RGBA {
+	new_image := image.NewRGBA(image_data.Bounds())
+	draw.Draw(new_image, image_data.Bounds(), image_data, image.Point{}, draw.Over)
 	
-	data, err := os.Open(args[1])
-	if err != nil {
-		log.Fatal("Could not open image: ", err)
-	}
-	defer data.Close()
-	
-	data_image, format_name, err := image.Decode(data)
-	if err != nil {
-		log.Fatal("Could not decode jpeg image: ", err)
-	}
-	
-	log.Println("Format name:", format_name)
-	log.Println("Image bounds:", data_image.Bounds().Max)
-
-	printPixels(data_image)
-	// r, g, b, a := data_image.Bounds().At(639, 800).RGBA()
-	// log.Println(r,g,b,a)
-	
+	//FIXME
+	//make sure this is fine, should be because image.Image holds a pointer
+	//to the actual data
+	return *new_image;
 }
